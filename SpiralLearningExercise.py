0735adadea99c1e9ec8f1a77c37912840704e3ca
DubdeeB <ligolas@gmail.com>
2021-12-02 11:38:49 +0200

    Working linear classifier example

--- SpiralLearningExercise.py
+++ SpiralLearningExercise.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def generate_data(points_per_class=100, dim=2, num_classes=4, spiral_angle_span=2 * np.pi, spiral_width=np.pi / 20,
+def generate_data(points_per_class=100, dim=2, num_classes=3, spiral_angle_span=4, spiral_width=0.1,
                   plot=True):
     X = np.zeros((points_per_class * num_classes, dim))  # data matrix (each row = single example)
     y = np.zeros(points_per_class * num_classes, dtype='uint8')  # class labels
@@ -15,29 +15,30 @@ def generate_data(points_per_class=100, dim=2, num_classes=4, spiral_angle_span=
         y[ix] = j
     if plot:
         plt.scatter(X[:, 0], X[:, 1], c=y, s=40)  # , cmap=plt.cm.Spectral)
-        plt.show()
+        plt.title('2D Spiral classification challenge')
     return X, y
 
 
 def Loss_function(true_classification, scores, weights, lambda_regularization, loss_type='SVM',
                   regularization_penalty='L2'):
     N = len(true_classification)
+    classification_indices = (range(N), true_classification)
     if loss_type == 'SVM':
-        margin = (scores.T - scores[range(N), true_classification]).T + 1
+        margin = (scores.T - scores[classification_indices]).T + 1
         L = 1 / N * (np.sum(np.maximum(0, margin))) - 1
         # in CS231n they use sum over j ~= true_classification, which is equivalent tp subtracting the margin once
         # for margin>0
         indicator = np.zeros(margin.shape)
         indicator[margin > 0] = 1
         dscores = np.copy(indicator)
-        dscores[range(N), true_classification] = 1 - np.sum(indicator, 1)
+        dscores[classification_indices] = 1 - np.sum(indicator, 1)
         dscores /= N
     elif loss_type == 'Softmax':
         exp_scores = np.exp((scores.T - np.max(scores, 1)).T)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-        L = 1 / N * np.sum(-np.log(probs[range(N), true_classification]))
+        L = 1 / N * np.sum(-np.log(probs[classification_indices]))
         dscores = probs
-        dscores[range(N), true_classification] -= 1
+        dscores[classification_indices] -= 1
         dscores /= N
     else:
         raise NotImplementedError
@@ -50,9 +51,68 @@ def Loss_function(true_classification, scores, weights, lambda_regularization, l
     return L, dscores, dW_regularization
 
 
+def Linear_classifier(X, W, b):
+    return np.dot(X, W) + b
+
+
+def Linear_classifier_backpropagation(X, W, b, dscores):
+    dW = np.dot(X.T, dscores)
+    db = np.sum(dscores, axis=0, keepdims=True)
+    return dW, db
+
+
+def initialize_linear_W(dim, num_classes):
+    W = 0.01 * np.random.randn(dim, num_classes)
+    b = np.zeros((1, num_classes))
+    return W, b
+
+
+def train(iterations, X, y, lambda_regularization, dim, num_classes, classification_type='Linear'):
+    if classification_type == 'Linear':
+        W, b = initialize_linear_W(dim, num_classes)
+    else:
+        raise NotImplementedError
+    losses = []
+    for i in range(iterations):
+        if classification_type == 'Linear':
+            scores = Linear_classifier(X, W, b)
+        else:
+            raise NotImplementedError
+        L, dscores, dW_regularization = Loss_function(y, scores, W, lambda_regularization)
+        losses.append(L)
+        if classification_type == 'Linear':
+            dW, db = Linear_classifier_backpropagation(X, W, b, dscores)
+        else:
+            raise NotImplementedError
+        dW += dW_regularization
+        W += -grad_step * dW
+        b += -grad_step * db
+    plt.figure()
+    plt.plot(losses, '.', label=classification_type + ' classifier with SVM loss')
+    plt.xlabel('iterations')
+    plt.ylabel('loss')
+    plt.grid()
+    predicted_class = np.argmax(scores, axis=1)
+    print('Linear classifier training accuracy: %.2f' % (np.mean(predicted_class == y)))
+
+
 if __name__ == "__main__":
     """
     Exercise follows stanford course CS231n: https://cs231n.github.io/neural-networks-case-study/
     """
-    X, y = generate_data()
-    print("Finished training process")
+    dim, num_classes = 2, 3
+    iterations = 100
+    grad_step = 0.2
+    lambda_regularization = 1e-3
+
+    size = 30
+    plt.rcParams.update({'legend.fontsize': size * 0.75, 'figure.figsize': (12, 9), 'axes.labelsize': size,
+                         'axes.titlesize': size,
+                         'xtick.labelsize': size * 0.75, 'ytick.labelsize': size * 0.75})
+
+    X, y = generate_data(dim=dim, num_classes=num_classes)
+
+    train(iterations, X, y, lambda_regularization, dim, num_classes, classification_type='Linear')
+
+    plt.legend()
+    plt.show()
